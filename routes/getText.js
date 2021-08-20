77b720dd9cb7b24735c8bf814bd681d8310a51fe
DeitySama <deitymills@outlook.com>
2021-08-20 07:48:43 +0000

    Init

--- routes/getText.js
+++ routes/getText.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router  = express.Router();
-const {getText} = require('../controllers/getText.js');
+
+const { fromPath } = require("pdf2pic");
+
+const  {createWorker} = require('tesseract.js');
+const worker = createWorker({
+    logger:m=>console.log(m)
+});
+
 const  multer = require("multer"); 
+const fs = require('fs');
 
 let storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -15,11 +23,46 @@ let storage = multer.diskStorage({
 let upload = multer({ storage: storage });
 
 router.post("/getText",upload.single("file"),async(req,res,next)=>{
-    
-    console.log(req.file);
-    res.status(200).json({
-        msg:"Fire ooooooooooooo"
-    })
+    pdf2pic(req.file.path)
 });
+
+const tesseractify = (file)=>{
+    fs.readFile(req.file.path,async(err,data)=>{
+        if(err) return err;
+        console.log(data);
+
+        (async () => {
+            await worker.load();
+            await worker.loadLanguage('eng');
+            await worker.initialize('eng');
+            const { data: { text } } = await worker.recognize(data);
+            res.json({
+                msg:'SUCCESS',
+                text
+            })
+            await worker.terminate();
+          })();
+
+    })
+}
+
+const pdf2pic = (path) =>{
+    const options = {
+        density: 100,
+        saveFilename: "untitled",
+        savePath: "./tempImages",
+        format: "png",
+        width: 600,
+        height: 600
+      };
+      const storeAsImage = fromPath(path, options);
+      const pageToConvertAsImage = 1;
+      
+      storeAsImage(pageToConvertAsImage).then((resolve) => {
+        console.log("PDF has Been converted to images");
+      
+        return resolve;
+      });
+}
 
 module.exports = router;
